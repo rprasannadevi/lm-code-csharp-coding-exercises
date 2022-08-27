c727ce8923a1caaab1f488ba4bcbfa0638de6ff2
Prasanna <prasannadevi@gmail.com>
2022-08-27 22:10:03 +0100

    Modified file to code and test Exercise003

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using Exercises.Models;
 namespace Exercises
 {
-    public class Exercise003
-    {
-        // Replace the exception statement below with your code!
-        public string[] IceCreamFlavours => throw new NotImplementedException();
+	public class Exercise003
+	{
+        public string[] IceCreamFlavours(List<IceCream> icecreams)
+		{
+            //Console.WriteLine(icecreams.Count);
+            string[] iceCreamFlavour = new string[icecreams.Count];
+			int i = 0;
+            foreach (var ice in icecreams)
+                iceCreamFlavour[i++] = ice.IceCreamFlavour;
+            return iceCreamFlavour;
 
-        // Replace the exception statement below with your code!
-        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
-    }
+        }
+
+         public int IceCreamCode(string iceCreamFlavour, List<IceCream> icecreams)
+        {
+            int iceCreamCode = 0;
+            foreach (var ice in icecreams)
+            {
+                if (ice.IceCreamFlavour.Equals(iceCreamFlavour))
+                    iceCreamCode = ice.IceCreamCode;
+                    
+            }
+            return iceCreamCode;
+        }
+
+        public int IceCreamCode(string iceCreamFlavour)
+		{
+            if (iceCreamFlavour.Equals("Mint Chocolate Chip"))
+				return 3;
+			else if (iceCreamFlavour.Equals("Mango Sorbet"))
+				return 5;
+			else if (iceCreamFlavour.Equals("Raspberry Ripple"))
+				return 1;
+			else
+				return 0;
+		}
+
+	}
 }
