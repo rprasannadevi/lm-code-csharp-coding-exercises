f6cf3b1e3e0cfba69e7ba88935f86a3797341c4b
Prasanna <prasannadevi@gmail.com>
2022-08-27 22:10:28 +0100

    Modified file to code and test Exercise005

--- Exercises/Exercise005.cs
+++ Exercises/Exercise005.cs
@@ -5,8 +5,31 @@ namespace Exercises
     {
         public bool IsPangram(string input)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            var isPangram = false;
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                string toCheckPangram = "abcdefghijklmnopqrstuvwxyz";
+                int count = 0;
+                foreach(var c in toCheckPangram)
+                {
+                    foreach(var ch in input.ToLower())
+                    {
+                        //Console.WriteLine(ch);
+                        //Console.WriteLine(c);
+                        if (c == ch)
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                if (count == 26)
+                    isPangram =  true;
+             
+            }
+
+            return isPangram;
         }
     }
 }
