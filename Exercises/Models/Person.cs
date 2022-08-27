21043304971d0c954a097da3e42adfb348827ba1
Prasanna <prasannadevi@gmail.com>
2022-08-27 22:05:15 +0100

    Added Person.cs to complete Exercise002

---
+++ Exercises/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Models
+{
+	public class Person
+	{
+		public Person(string firstName, string lastName, string city, int age)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			City = city;
+			Age = age;
+		}
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string City { get; set; }
+
+		public int Age { get; set; }
+	}
+}
