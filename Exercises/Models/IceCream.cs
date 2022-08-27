3a226a7bbe6957a867da54aba4030a45f2d6fa01
Prasanna <prasannadevi@gmail.com>
2022-08-27 22:04:17 +0100

    Added IceCream.cs to complete Exercise003

---
+++ Exercises/Models/IceCream.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Models
+{
+	public class IceCream
+	{
+		public IceCream(string iceCreamFlavour, int iceCreamCode)
+		{
+			IceCreamFlavour = iceCreamFlavour;
+			IceCreamCode = iceCreamCode;
+		}
+
+		public string IceCreamFlavour { get; set; }
+
+		public int IceCreamCode { get; set; }
+	}
+}
